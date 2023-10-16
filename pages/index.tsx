fd32cef1630f74331c81e0c71a72fcf8e2607568
Joe Friesen <friesen.josephc@gmail.com>
2023-10-16 11:03:04 -0700

    remove posts, api index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,8 @@
 import Head from "next/head";
-import Link from "next/link";
-import { GetStaticProps } from "next";
-import { getSortedPostsData } from "../lib/posts";
-import Date from "../components/date";
 import Layout, { SITE_TITLE } from "../components/layout";
 import utilStyles from "../styles/utils.module.sass";
 
-export const getStaticProps: GetStaticProps = async () => {
-  const allPostsData = getSortedPostsData();
-
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-};
-
-export default function Home({ allPostsData }: {
-  allPostsData: {
-    date: string;
-    title: string;
-    id: string;
-  }[]
-}) {
+export default function Home() {
   return (
     <Layout home>
       <Head>
@@ -30,20 +10,6 @@ export default function Home({ allPostsData }: {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>hi.</p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
       </section>
     </Layout>
   );
